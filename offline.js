a40cc71b63fd8010e2d6a76546ef4a601fee7473
Igor Miranda Moreira <igormm2001@gmail.com>
2021-06-14 19:44:28 -0300

    exportacao html

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1623710634,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/fundo.png",
+		"images/botao.png",
+		"media/spawn.m4a",
+		"media/spawn.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"doctor_glitch-webfont.eot",
+		"doctor_glitch-webfont.svg",
+		"doctor_glitch-webfont.ttf",
+		"doctor_glitch-webfont.woff",
+		"doctor_glitch-webfont.woff2",
+		"stylesheet.css"
+	]
+}
